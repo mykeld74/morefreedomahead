86009dab60e33afb04c95e7deff9ff79b155ea47
mykeld74 <mike@msdwebsolutions.com>
2026-05-08 09:58:22 -0600

    fix deployment take 2

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,16 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
+import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const fallbackBuildDatabaseUrl =
+	'postgresql://build:build@localhost:5432/build_placeholder?sslmode=disable';
 
-const client = neon(env.DATABASE_URL);
+const databaseUrl = env.DATABASE_URL ?? (building ? fallbackBuildDatabaseUrl : undefined);
+
+if (!databaseUrl) throw new Error('DATABASE_URL is not set');
+
+const client = neon(databaseUrl);
 
 export const db = drizzle(client, { schema });
